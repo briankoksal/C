76b4bcbce4a70085a8d577d8634f5105cd829e22
heuristack <liying.hku@gmail.com>
2014-09-04 15:05:34 +0800

    home

--- Tests/kr5c.b.c
+++ Tests/kr5c.b.c
@@ -1,2 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
+#define MAXTOKEN 100
 
+enum { NAME, PARENS, BRACKETS };
+
+int gettoken(void);
+
+int tokentype; 				/* type of last token */
+char token[MAXTOKEN]; 		/* last token string */
+char name[MAXTOKEN]; 		/* identifier name */
+char datatype[MAXTOKEN]; 	/* data type = char, int, etc. */
+char out[1000];
+
+#define BUFSIZE 100
+char buf[BUFSIZE]; 		/* buffer for ungetch */
+int bufp = 0; 			/* next free position in buf */
+
+int getch(void) 		/* get a (possibly pushed-back) character */
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)	/* push character back on input */
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
+int gettoken(void) /* return next token */
+{
+	int c, getch(void);
+	void ungetch(int);
+	char *p = token;
+
+	while ((c = getch()) == ' ' || c == '\t')
+		;
+
+	if (c == '(') {
+		if ((c = getch()) == ')') {
+			strcpy(token, "()");
+			return tokentype = PARENS;
+		} else {
+			ungetch(c);
+			return tokentype = '(';
+		}
+	} else if (c == '[') {
+		for (*p++ = c; (*p++ = getch()) != ']'; )
+			;
+		*p = '\0';
+		return tokentype = BRACKETS;
+	} else if (isalpha(c)) {
+		for (*p++ = c; isalnum(c = getch()); )
+			*p++ = c;
+		*p = '\0';
+		ungetch(c);
+		return tokentype = NAME;
+	} else
+		return tokentype = c;
+}
+
+/* undcl: convert word descriptions to declarations */
+main()
+{
+
+	int type;
+	char temp[MAXTOKEN];
+
+	while (gettoken() != EOF) {
+
+		strcpy(out, token);
+
+		while ((type = gettoken()) != '\n')
+			if (type == PARENS || type == BRACKETS)
+				strcat(out, token);
+			else if (type == '*') {
+				sprintf(temp, "(*%s)", out);
+				strcpy(out, temp);
+			} else if (type == NAME) {
+				sprintf(temp, "%s %s", token, out);
+				strcpy(out, temp);
+			} else
+				printf("invalid input at %s\n", token);
+	}
+	return 0;
+}
